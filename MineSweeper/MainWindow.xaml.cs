5d6d6213eeb1e8d0972f6feba39d0966c2acf58b
Seng Fai Chan <SChan@spartaglobal.com>
2019-09-06 16:19:09 +0100

    Randomly place mine, determining which tile is safe or not, UI, determining which tile is on the edge

--- MineSweeper/MainWindow.xaml.cs
+++ MineSweeper/MainWindow.xaml.cs
@@ -18,144 +18,178 @@ namespace MineSweeper
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        Board board;
         public MainWindow()
         {
             InitializeComponent();
-            InitializeGrid(5, 5);
+            InitializeGame();
         }
 
-        private void InitializeGrid(int row, int colume)
+        void InitializeGame()
         {
-            int blockSize = 45;
-            // Create the Grid
-            Grid DynamicGrid = new Grid();
-            DynamicGrid.Width = row * blockSize;
-            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Left;
-            DynamicGrid.VerticalAlignment = VerticalAlignment.Top;
-            DynamicGrid.ShowGridLines = true;
-            DynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
+            board = new Board(9, 5);
+            this.Content = board.GameBoard;
+        }
+    }
 
-            // Create Columns
-            for (int i = 0; i < colume; i++)
+    public class Tile
+    {
+        Button button;
+        Board gameBoard;
+        int row;
+        int column;
+        int id;
+        bool hasMine;
+        public bool HasMine
+        {
+            get { return hasMine; }
+            set
             {
-                ColumnDefinition columnDef = new ColumnDefinition();
-                columnDef.Width = new GridLength(blockSize);
-                DynamicGrid.ColumnDefinitions.Add(columnDef);
-
-                Button btn = new Button();
-                //Grid.SetColumn(btn);
-
-
-
-                //int product = numbers.Aggregate(1, (interim, next) => interim * next);
-                //// Add first column header
-                //TextBlock txtBlock1 = new TextBlock();
-                //txtBlock1.Text = "Author Name";
-                //txtBlock1.FontSize = 14;
-                //txtBlock1.FontWeight = FontWeights.Bold;
-                //txtBlock1.Foreground = new SolidColorBrush(Colors.Green);
-                //txtBlock1.VerticalAlignment = VerticalAlignment.Top;
-                //Grid.SetRow(txtBlock1, 0);
-                //Grid.SetColumn(txtBlock1, 0);
+                hasMine = value;
+                button.Content = "M";
             }
+        }
+
+        public Tile(int row, int column, Board gameBoard)
+        {
+            this.column = column;
+            this.row = row;
+            this.gameBoard = gameBoard;
+            this.id = row * gameBoard.Column + column;
+            button = new Button();
+            hasMine = false;
+            button.Content = ((row * gameBoard.Column) + column).ToString();
+            button.Click += OnTileClick;
+            Grid.SetColumn(this.button, column);
+            Grid.SetRow(this.button, row);
+            gameBoard.GameBoard.Children.Add(button);
+        }
+
+        public void OnTileClick(object sender, RoutedEventArgs e)
+        {
+            //if (hasMine)
+            //    MessageBox.Show("Boom");
+            //else
+            //{
+            //    //MessageBox.Show("Safe");
+            //}
+
+            NeighbourNumber(id, gameBoard.Row, gameBoard.Column);
+
+        }
+
+        List<int> NeighbourNumber(int i, int row, int column)
+        {
+            int x = i % row;
+            int y = i / column;
+
+            List<int> numbers = new List<int>();
+            //Top row
+            if (y == 0)
+                MessageBox.Show("TopRow");
+            if (y == row-1)
+                MessageBox.Show("BottomRow");
+            if (y == 0)
+                MessageBox.Show("LeftColumn");
+            if (y == column - 1)
+                MessageBox.Show("RightColumn");
+            return numbers;
+        }
+
+        List<Tile> GetNeighbourTile()
+        {
+            List<Tile> tiles = new List<Tile>();
+            List<int> numbers = NeighbourNumber(id, gameBoard.Row, gameBoard.Column);
+            foreach (int i in numbers)
+            {
+                tiles.Add(gameBoard.Tiles[i]);
+            }
+
+            return tiles;
+        }
+    }
+
+    public class Board
+    {
+        List<Tile> tiles;
+        public List<Tile> Tiles {get{return tiles;} }
+        Grid gameBoard;
+        public Grid GameBoard
+        {
+            get { return gameBoard; }
+        }
+
+        public int Row { get { return row; } set { row = value; }  }
+        public int Column { get { return column; }  set { column = value; } }
+
+        int row;
+        int column;
+
+        public Board(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            tiles = new List<Tile>();
+            gameBoard = new Grid();
+            int blockSize = 45;
+
+            // Create the Grid
+            gameBoard = new Grid();
+            gameBoard.Width = column * blockSize;
+            gameBoard.Height = row * blockSize;
+            gameBoard.HorizontalAlignment = HorizontalAlignment.Left;
+            gameBoard.VerticalAlignment = VerticalAlignment.Top;
+            gameBoard.ShowGridLines = true;
+            gameBoard.Background = new SolidColorBrush(Colors.LightSteelBlue);
+
 
             //Create rows
             for (int i = 0; i < row; i++)
             {
                 RowDefinition rowDef = new RowDefinition();
                 rowDef.Height = new GridLength(blockSize);
-                DynamicGrid.RowDefinitions.Add(rowDef);
+                gameBoard.RowDefinitions.Add(rowDef);
 
-                //// Create first Row
-                //TextBlock authorText = new TextBlock();
-                //authorText.Text = "Mahesh Chand";
-                //authorText.FontSize = 12;
-                //authorText.FontWeight = FontWeights.Bold;
-                //Grid.SetRow(authorText, 1);
-                //Grid.SetColumn(authorText, 0);
-                ////gridRow1.Height = new GridLength(45);
+                // Create Columns
+                for (int j = 0; j < column; j++)
+                {
+                    ColumnDefinition columnDef = new ColumnDefinition();
+                    columnDef.Width = new GridLength(blockSize);
+                    gameBoard.ColumnDefinitions.Add(columnDef);
+
+                    Tile tile = new Tile(i, j, this);
+                    tiles.Add(tile);
+                }
             }
+            SetMine(RandomNumber(10), tiles);
+        }
 
- 
+        public List<int> RandomNumber(int num_of_items)
+        {
+            List<int> numbers = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < num_of_items; i++)
+            {
+                int rand = random.Next(0, (row * column) - 1);
+                numbers.Add(rand);
+            }
+            return numbers;
+        }
 
-
-            //// Add second column header
-            //TextBlock txtBlock2 = new TextBlock();
-            //txtBlock2.Text = "Age";
-            //txtBlock2.FontSize = 14;
-            //txtBlock2.FontWeight = FontWeights.Bold;
-            //txtBlock2.Foreground = new SolidColorBrush(Colors.Green);
-            //txtBlock2.VerticalAlignment = VerticalAlignment.Top;
-            //Grid.SetRow(txtBlock2, 0);
-            //Grid.SetColumn(txtBlock2, 1);
-
-            //// Add third column header
-            //TextBlock txtBlock3 = new TextBlock();
-            //txtBlock3.Text = "Book";
-            //txtBlock3.FontSize = 14;
-            //txtBlock3.FontWeight = FontWeights.Bold;
-            //txtBlock3.Foreground = new SolidColorBrush(Colors.Green);
-            //txtBlock3.VerticalAlignment = VerticalAlignment.Top;
-            //Grid.SetRow(txtBlock3, 0);
-            //Grid.SetColumn(txtBlock3, 2);
-
-            ////// Add column headers to the Grid
-            //DynamicGrid.Children.Add(txtBlock1);
-            //DynamicGrid.Children.Add(txtBlock2);
-            //DynamicGrid.Children.Add(txtBlock3);
-
-
-
-            //TextBlock ageText = new TextBlock();
-            //ageText.Text = "33";
-            //ageText.FontSize = 12;
-            //ageText.FontWeight = FontWeights.Bold;
-            //Grid.SetRow(ageText, 1);
-            //Grid.SetColumn(ageText, 1);
-
-            //TextBlock bookText = new TextBlock();
-            //bookText.Text = "GDI+ Programming";
-            //bookText.FontSize = 12;
-            //bookText.FontWeight = FontWeights.Bold;
-            //Grid.SetRow(bookText, 1);
-            //Grid.SetColumn(bookText, 2);
-
-            //// Add first row to Grid
-            //DynamicGrid.Children.Add(authorText);
-            //DynamicGrid.Children.Add(ageText);
-            //DynamicGrid.Children.Add(bookText);
-
-            //// Create second row
-            //authorText = new TextBlock();
-            //authorText.Text = "Mike Gold";
-            //authorText.FontSize = 12;
-            //authorText.FontWeight = FontWeights.Bold;
-            //Grid.SetRow(authorText, 2);
-            //Grid.SetColumn(authorText, 0);
-            //ageText = new TextBlock();
-            //ageText.Text = "35";
-            //ageText.FontSize = 12;
-            //ageText.FontWeight = FontWeights.Bold;
-            //Grid.SetRow(ageText, 2);
-            //Grid.SetColumn(ageText, 1);
-            //bookText = new TextBlock();
-            //bookText.Text = "Programming C#";
-            //bookText.FontSize = 12;
-            //bookText.FontWeight = FontWeights.Bold;
-            //Grid.SetRow(bookText, 2);
-            //Grid.SetColumn(bookText, 2);
-
-            //// Add second row to Grid
-            //DynamicGrid.Children.Add(authorText);
-            //DynamicGrid.Children.Add(ageText);
-            //DynamicGrid.Children.Add(bookText);
-
-            // Display grid into a Window
-            this.Content = DynamicGrid;
-
-
+        public void SetMine(List<int> numbers, List<Tile> tiles)
+        {
+            foreach (int i in numbers)
+            {
+                tiles[i].HasMine = true;
+            }
         }
     }
 }
